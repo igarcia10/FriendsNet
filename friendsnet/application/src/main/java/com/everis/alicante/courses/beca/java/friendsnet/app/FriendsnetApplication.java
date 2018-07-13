b742666bacdcde70a544664ea671580d3d21097f
igarcia10 <40008812+igarcia10@users.noreply.github.com>
2018-07-13 17:21:31 +0200

    config added
    ready to start coding

--- friendsnet/application/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/app/FriendsnetApplication.java
+++ friendsnet/application/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/app/FriendsnetApplication.java
@@ -1,16 +1,18 @@
-package com.everis.alicante.courses.beca.java.friendsnet;
+package com.everis.alicante.courses.beca.java.friendsnet.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.everis.alicante.courses.beca.java.friendsnet"})
 @EntityScan("com.everis.alicante.courses.beca.java.friendsnet.persistence.entity")
-public class ITApplication {
+@EnableJpaRepositories("com.everis.alicante.courses.beca.java.friendsnet.persistence.dao")
+public class FriendsnetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ITApplication.class, args);
+		SpringApplication.run(FriendsnetApplication.class, args);
 	}
 }
