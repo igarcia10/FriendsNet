97d9b0b3b4b7d1b1b3eb4e256d67422981610b36
ivangarcia <ivangarciatecnico@gmail.com>
2018-07-17 10:41:32 +0200

    Deleted Like Manager

--- friendsnet/core/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/core/manager/LikeManager.java
+++
@@ -1,7 +0,0 @@
-package com.everis.alicante.courses.beca.java.friendsnet.core.manager;
-
-import com.everis.alicante.courses.beca.java.friendsnet.persistence.entity.Like;
-
-public interface LikeManager extends Manager<Like, Long> {
-
-}
