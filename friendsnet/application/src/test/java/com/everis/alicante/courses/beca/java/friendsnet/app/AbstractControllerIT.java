4f1b29c902b0976e37cc13c079c8871ac53c485e
igarcia10 <40008812+igarcia10@users.noreply.github.com>
2018-07-19 16:41:37 +0200

    finished abstract ControllerIT and PersonControllerIT

--- friendsnet/application/src/test/java/com/everis/alicante/courses/beca/java/friendsnet/app/AbstractControllerIT.java
+++ friendsnet/application/src/test/java/com/everis/alicante/courses/beca/java/friendsnet/app/AbstractControllerIT.java
@@ -2,6 +2,7 @@ package com.everis.alicante.courses.beca.java.friendsnet.app;
 
 import org.json.JSONException;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,23 +15,32 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import com.everis.alicante.courses.beca.java.friendsnet.persistence.entity.FNEntity;
+import com.everis.alicante.courses.beca.java.friendsnet.service.dto.DTOEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
-public abstract class AbstractControllerIT<E, ID> {
+
+public abstract class AbstractControllerIT<DTO extends DTOEntity, E extends FNEntity, ID> {
 
 	@LocalServerPort
 	private int port;
 
 	abstract CrudRepository<E, Long> getDAO();
 
-	protected String URI;
-	protected String findAllResult;
+	private String URI;
+	private String findAllResult;
+	private String findByIdResult;
+	private String afterCreateResult;
+	private Class<DTO> dtoClass;
 
-	public AbstractControllerIT(String URI, String findAllResult) {
+	public AbstractControllerIT(String URI, String findAllResult, String findByIdResult, String afterCreateResult, Class<DTO> dtoClass) {
 		this.URI = URI;
 		this.findAllResult = findAllResult;
+		this.findByIdResult = findByIdResult;
+		this.afterCreateResult = afterCreateResult;
+		this.dtoClass = dtoClass;
 	}
 
 	TestRestTemplate restTemplate = new TestRestTemplate();
@@ -59,7 +69,7 @@ public abstract class AbstractControllerIT<E, ID> {
 	}
 
 	@Test
-	@DatabaseSetup("/db/person/init.xml")
+	@DatabaseSetup("/db/abstract/init.xml")
 	public void testFindAllWithContent() throws JSONException {
 		// Act
 		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI), HttpMethod.GET, null,
@@ -67,6 +77,80 @@ public abstract class AbstractControllerIT<E, ID> {
 
 		// Assert
 		JSONAssert.assertEquals(findAllResult, response.getBody(), false);
+	}
+
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testFindById() throws JSONException {
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI + "/1"), HttpMethod.GET, null,
+				String.class);
+
+		// Assert
+		JSONAssert.assertEquals(findByIdResult, response.getBody(), false);
+	}
+
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testFindByIdNotInDb() throws JSONException {
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI + "/3"), HttpMethod.GET, null,
+				String.class);
+
+		// Assert
+		JSONAssert.assertEquals(null, response.getBody(), false);
+	}
+
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testCreate() throws Exception {
+		//Arrange
+		DTO dto = dtoClass.newInstance();
+		final Long counter = this.getDAO().count();
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI), HttpMethod.POST, this.getPostRequest(dto),
+				String.class);
+		// Assert
+		JSONAssert.assertEquals(afterCreateResult, response.getBody(), false);
+		Assert.assertEquals(counter+1, this.getDAO().count());
+	}
+	
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testCreateNull() throws Exception {
+		//Arrange
+		DTO dto = null;
+		final Long counter = this.getDAO().count();
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI), HttpMethod.POST, this.getPostRequest(dto),
+				String.class);
+		// Assert
+		JSONAssert.assertEquals("{}", response.getBody(), false);
+		Assert.assertEquals(counter.longValue(), this.getDAO().count());
+	}
+	
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testRemove() throws JSONException {
+		//Arrange
+		final Long counter = this.getDAO().count();
+		//Act
+		@SuppressWarnings("unused")
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI+"/2"), HttpMethod.DELETE, null, String.class);
+		//Assert
+		Assert.assertEquals(counter-1, this.getDAO().count());
+	}
+	
+	@Test
+	@DatabaseSetup("/db/abstract/init.xml")
+	public void testRemoveNotInDb() throws JSONException {
+		//Arrange
+		final Long counter = this.getDAO().count();
+		//Act
+		@SuppressWarnings("unused")
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI+"/3"), HttpMethod.DELETE, null, String.class);
+		//Assert
+		Assert.assertEquals(counter.longValue(), this.getDAO().count());
 	}
 
 	protected String createURLWithPort(String uri) {
--- friendsnet/application/src/test/java/com/everis/alicante/courses/beca/java/friendsnet/app/PersonControllerIT.java
+++ friendsnet/application/src/test/java/com/everis/alicante/courses/beca/java/friendsnet/app/PersonControllerIT.java
@@ -29,10 +29,13 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
 		TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
-public class PersonControllerIT extends AbstractControllerIT<Person, Long> {
+public class PersonControllerIT extends AbstractControllerIT<PersonDTO, Person, Long> {
 
 	public PersonControllerIT() {
-		super("/persons", "[{'id': 1, 'name':'name1', 'surname':'surname1'}, {'id': 2, 'name':'name2', 'surname':'surname2'}]");
+		super("/persons",
+				"[{'id': 1, 'name':'name1', 'surname':'surname1'}, {'id': 2, 'name':'name2', 'surname':'surname2'}]",
+				"{'id': 1, 'name':'name1', 'surname':'surname1'}", "{'id': 3, 'name':null, 'surname':null}",
+				PersonDTO.class);
 	}
 
 	@Autowired
@@ -56,7 +59,7 @@ public class PersonControllerIT extends AbstractControllerIT<Person, Long> {
 		dto.setName("name2");
 		dto.setSurname("surname2");
 		// Act
-		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI + "/1/relate"), HttpMethod.POST,
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/persons/1/relate"), HttpMethod.POST,
 				this.getPostRequest(dtos), String.class);
 
 		// Assert
@@ -67,7 +70,39 @@ public class PersonControllerIT extends AbstractControllerIT<Person, Long> {
 
 	@Test
 	@DatabaseSetup("/db/person/init.xml")
+	public void testRelateNull() throws JSONException {
+		// Arrange
+		List<PersonDTO> dtos = new ArrayList<>();
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/persons/1/relate"), HttpMethod.POST,
+				this.getPostRequest(dtos), String.class);
+
+		// Assert
+		JSONAssert.assertEquals("{'id': 1, 'name':'name1', 'surname':'surname1'}", response.getBody(),
+				JSONCompareMode.LENIENT);
+	}
+
+	@Test
+	@DatabaseSetup("/db/person/init.xml")
 	public void testRelatePersonNotInDb() throws JSONException {
+		// Arrange
+		List<PersonDTO> dtos = new ArrayList<>();
+		PersonDTO dto = new PersonDTO();
+		dtos.add(dto);
+		dto.setId(2L);
+		dto.setName("name2");
+		dto.setSurname("surname2");
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/persons/3/relate"), HttpMethod.POST,
+				this.getPostRequest(dtos), String.class);
+
+		// Assert
+		JSONAssert.assertEquals("{}", response.getBody(), JSONCompareMode.LENIENT);
+	}
+
+	@Test
+	@DatabaseSetup("/db/person/init.xml")
+	public void testRelateFriendsNotInDb() throws JSONException {
 		// Arrange
 		List<PersonDTO> dtos = new ArrayList<>();
 		PersonDTO dto = new PersonDTO();
@@ -76,12 +111,36 @@ public class PersonControllerIT extends AbstractControllerIT<Person, Long> {
 		dto.setName("name3");
 		dto.setSurname("surname3");
 		// Act
-		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort(URI + "/1/relate"), HttpMethod.POST,
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/persons/1/relate"), HttpMethod.POST,
 				this.getPostRequest(dtos), String.class);
 
 		// Assert
 		JSONAssert.assertEquals("{'id': 1, 'name':'name1', 'surname':'surname1'}", response.getBody(),
 				JSONCompareMode.LENIENT);
+	}
+
+	@Test
+	@DatabaseSetup("/db/person/init.xml")
+	public void testRelateFriendNotInDb() throws JSONException {
+		// Arrange
+		List<PersonDTO> dtos = new ArrayList<>();
+		PersonDTO dto1 = new PersonDTO();
+		PersonDTO dto2 = new PersonDTO();
+		dtos.add(dto1);
+		dto1.setId(2L);
+		dto1.setName("name2");
+		dto1.setSurname("surname2");
+		dto2.setId(3L);
+		dto2.setName("name3");
+		dto2.setSurname("surname3");
+		// Act
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/persons/1/relate"), HttpMethod.POST,
+				this.getPostRequest(dtos), String.class);
+
+		// Assert
+		JSONAssert.assertEquals(
+				"{'friends':[{'id': 2, 'name':'name2', 'surname':'surname2'}], 'id': 1, 'name':'name1', 'surname':'surname1'}",
+				response.getBody(), JSONCompareMode.LENIENT);
 	}
 
 }
