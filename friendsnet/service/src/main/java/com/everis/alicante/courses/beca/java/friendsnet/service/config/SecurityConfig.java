14e7f1995835c7aca18a0688aa054b2901c77e98
ivangarcia <ivangarciatecnico@gmail.com>
2018-08-03 00:44:26 +0200

    fixed swagger 404 problem

--- friendsnet/service/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/service/config/SecurityConfig.java
+++ friendsnet/service/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/service/config/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.everis.alicante.courses.beca.java.friendsnet.service.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests().antMatchers("/**").permitAll();
-	}
+    private static final String[] AUTH_WHITELIST = {
 
-	
-	
+            // -- swagger ui
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**",
+            "/**"
+    };
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers("/**/*").denyAll();
+    }
 }
+
+
