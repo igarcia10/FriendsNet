9bd443b42144d685ebfe8901169997d312818ed8
ivangarcia <ivangarciatecnico@gmail.com>
2018-08-03 02:05:43 +0200

    added post and delete URIs to security

--- friendsnet/service/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/service/config/SecurityConfig.java
+++ friendsnet/service/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/service/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.everis.alicante.courses.beca.java.friendsnet.service.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] AUTH_WHITELIST = {
+    private static final String[] WHITELIST = {
 
             // -- swagger ui
             "/swagger-resources/**",
@@ -22,10 +24,25 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
             
     };
     
+    private static final String[] POST_WHITELIST = {
+
+            "/persons/**",
+            "/persons/**/relate",
+            "/posts",
+            "/posts/**/person/**/like/**"
+            
+    };
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(AUTH_WHITELIST).permitAll();
+                .antMatchers(WHITELIST).permitAll();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, POST_WHITELIST)
+        	.and().ignoring().antMatchers(HttpMethod.DELETE, "/posts/**");
     }
 }
 
